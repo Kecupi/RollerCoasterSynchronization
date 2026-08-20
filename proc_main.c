80d4c90d698629b3ed9a5974b674203f6435ddee
Stepan Horenek <stepan.horenek@gmail.com>
2026-08-20 18:04:39 +0200

    Added missing check after 'ftruncate' call, added helper functions 'init_shared_values', 'destroy_semaphores', 'destroy_shared_memory' in 'proc_main'

--- proc_main.c
+++ proc_main.c
@@ -18,6 +18,50 @@
 #include "proc_input.h"
 #include "proc_entities.h"
 
+/**
+ *  @brief Function for initialization of values in shared memory
+ *  @param shared_mem pointer to shared structure to be initialized
+*/
+void init_shared_values(shared_struct* shared_mem){
+    shared_mem->actions_cnt = 0;
+    shared_mem->visitor_cnt = 0;
+    shared_mem->cart_cnt = 0;
+    shared_mem->next_cart_cnt = 0;
+    sem_init(&shared_mem->write_mutex, 1, 1);
+    sem_init(&shared_mem->next_cart, 1, 0);
+    sem_init(&shared_mem->cart_disp, 1, 0);
+    sem_init(&shared_mem->turnstile_enter, 1, 0);
+    sem_init(&shared_mem->turnstile_leave, 1, 0);
+    sem_init(&shared_mem->boarding, 1, 0);
+    sem_init(&shared_mem->unboarding, 1, 0);
+}
+
+/**
+ *  @brief Function for destruction of semaphore files
+ *  @param shared_mem pointer to shared structure to be initialized
+*/
+void destroy_semaphores(shared_struct* shared_mem){
+    sem_destroy(&shared_mem->write_mutex);
+    sem_destroy(&shared_mem->next_cart);
+    sem_destroy(&shared_mem->cart_disp);
+    sem_destroy(&shared_mem->turnstile_enter);
+    sem_destroy(&shared_mem->turnstile_leave);
+    sem_destroy(&shared_mem->boarding);
+    sem_destroy(&shared_mem->unboarding);
+}
+
+/**
+ *  @brief Function for destruction of shared memory
+ *  @param shared_name text name of shared memory segment
+ *  @param shared_id id of descriptor returned by shm_open()
+ *  @param shared_mem pointer to shared structure to be initialized
+*/
+void destroy_shared_memory(char* shared_name, int shared_id, shared_struct* shared_mem){
+    munmap(shared_mem, sizeof(shared_struct));
+    close(shared_id);
+    shm_unlink(shared_name);
+}
+
 int main (int argc, char** argv){
     sync_config* config = malloc(sizeof(sync_config));
     if (config == NULL){
@@ -36,49 +80,39 @@ int main (int argc, char** argv){
     }
     // shared memory initialization
     const char* shared_name = "/shared_info";
-    int shared = shm_open(shared_name, O_RDWR | O_CREAT, 0666);
-    if (shared == -1){
+    int shared_id = shm_open(shared_name, O_RDWR | O_CREAT, 0666);
+    if (shared_id == -1){
         fprintf(stderr, "ERROR: Failed to allocate shared memory\n");
         free(config);
         fclose(output);
         exit(1);
     }
-    ftruncate(shared, sizeof(shared_struct));
+    if ((ftruncate(shared, sizeof(shared_struct))) == -1){
+        fprintf(stderr, "Error: Failed to size shared memory\n");
+        free(config);
+        close(shared_id);
+        shm_unlink(shared_name);
+        fclose(output);
+        exit(1);
+    }
     shared_struct* shared_mem = mmap(NULL, sizeof(shared_struct), PROT_READ | PROT_WRITE, MAP_SHARED, shared, 0);
     if (shared_mem == MAP_FAILED){
         fprintf(stderr, "Error: Failed to map shared memory\n");
         free(config);
-        close(shared);
+        close(shared_id);
+        shm_unlink(shared_name);
         fclose(output);
         exit(1);
     }
-    // init value of shared memory
-    shared_mem->actions_cnt = 0;
-    shared_mem->visitor_cnt = 0;
-    shared_mem->cart_cnt = 0;
-    shared_mem->next_cart_cnt = 0;
-    sem_init(&shared_mem->write_mutex, 1, 1);
-    sem_init(&shared_mem->next_cart, 1, 0);
-    sem_init(&shared_mem->cart_disp, 1, 0);
-    sem_init(&shared_mem->turnstile_enter, 1, 0);
-    sem_init(&shared_mem->turnstile_leave, 1, 0);
-    sem_init(&shared_mem->boarding, 1, 0);
-    sem_init(&shared_mem->unboarding, 1, 0);
+    init_shared_values(shared_mem);
     // create dispatcher process
     sem_wait(&shared_mem->write_mutex);
     pid_t disp_pid = fork();
     if (disp_pid < 0){
         fprintf(stderr, "ERROR: Failed to create dispatcher process\n");
-        sem_destroy(&shared_mem->write_mutex);
-        sem_destroy(&shared_mem->next_cart);
-        sem_destroy(&shared_mem->cart_disp);
-        sem_destroy(&shared_mem->turnstile_enter);
-        sem_destroy(&shared_mem->turnstile_leave);
-        sem_destroy(&shared_mem->boarding);
-        sem_destroy(&shared_mem->unboarding);
         free(config);
-        munmap(shared_mem, sizeof(shared_struct));
-        close(shared);
+        destroy_semaphores(shared_mem);
+        destroy_shared_memory(shared_name, shared_id, shared_mem);
         fclose(output);
         exit(1);
     } else if (disp_pid == 0){
@@ -91,16 +125,9 @@ int main (int argc, char** argv){
         if (cart_pid < 0){
             fprintf(stderr, "ERROR: Failed to create cart process\n");
             // TODO: signal already existing processes to abort
-            sem_destroy(&shared_mem->write_mutex);
-            sem_destroy(&shared_mem->next_cart);
-            sem_destroy(&shared_mem->cart_disp);
-            sem_destroy(&shared_mem->turnstile_enter);
-            sem_destroy(&shared_mem->turnstile_leave);
-            sem_destroy(&shared_mem->boarding);
-            sem_destroy(&shared_mem->unboarding);
             free(config);
-            munmap(shared_mem, sizeof(shared_struct));
-            close(shared);
+            destroy_semaphores(shared_mem);
+            destroy_shared_memory(shared_name, shared_id, shared_mem);
             fclose(output);
             exit(1);
         } else if (cart_pid == 0){
@@ -114,16 +141,9 @@ int main (int argc, char** argv){
         if (visitor_pid < 0){
             fprintf(stderr, "ERROR: Failed to create visitor process\n");
             // TODO: signal already existing processes to abort
-            sem_destroy(&shared_mem->write_mutex);
-            sem_destroy(&shared_mem->next_cart);
-            sem_destroy(&shared_mem->cart_disp);
-            sem_destroy(&shared_mem->turnstile_enter);
-            sem_destroy(&shared_mem->turnstile_leave);
-            sem_destroy(&shared_mem->boarding);
-            sem_destroy(&shared_mem->unboarding);
             free(config);
-            munmap(shared_mem, sizeof(shared_struct));
-            close(shared);
+            destroy_semaphores(shared_mem);
+            destroy_shared_memory(shared_name, shared_id, shared_mem);
             fclose(output);
             exit(1);
         } else if (visitor_pid == 0){
@@ -132,15 +152,7 @@ int main (int argc, char** argv){
     }
     waitpid(disp_pid, NULL, 0);
     free(config);
-    sem_destroy(&shared_mem->write_mutex);
-    sem_destroy(&shared_mem->next_cart);
-    sem_destroy(&shared_mem->cart_disp);
-    sem_destroy(&shared_mem->turnstile_enter);
-    sem_destroy(&shared_mem->turnstile_leave);
-    sem_destroy(&shared_mem->boarding);
-    sem_destroy(&shared_mem->unboarding);
-    munmap(shared_mem, sizeof(shared_struct));
-    close(shared);
-    shm_unlink(shared_name);
+    destroy_semaphores(shared_mem);
+    destroy_shared_memory(shared_name, shared_id, shared_mem);
     fclose(output);
 }
