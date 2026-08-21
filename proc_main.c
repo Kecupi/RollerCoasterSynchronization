4196862986fad5a34488166383fa934d210fc65a
Stepan Horenek <stepan.horenek@gmail.com>
2026-08-21 20:37:23 +0200

    Added error handle to case when one of 'visitor' processes fails to fork, added lists tracking of children PIDs in main process

--- proc_main.c
+++ proc_main.c
@@ -108,12 +108,31 @@ int main (int argc, char** argv){
     }
     // initialize values in shared memory
     init_shared_values(shared_mem);
+    pid_t* cart_pids = malloc(config->cart_num * sizeof(pid_t));
+    if (cart_pids == NULL){
+        fprintf(stderr, "ERROR: Failed to allocate memory for cart PIDs array\n");
+        free(config);
+        destroy_semaphores(shared_mem);
+        destroy_shared_memory(shared_name, shared_id, shared_mem);
+        exit(1);
+    }
+    pid_t* visitor_pids = malloc(config->visitor_num * sizeof(pid_t));
+    if (visitor_pids == NULL){
+        fprintf(stderr, "ERROR: Failed to allocate memory for visitor PIDs array\n");
+        free(config);
+        free(cart_pids);
+        destroy_semaphores(shared_mem);
+        destroy_shared_memory(shared_name, shared_id, shared_mem);
+        exit(1);
+    }
     // create dispatcher process
     sem_wait(&shared_mem->write_mutex);
     pid_t disp_pid = fork();
     if (disp_pid < 0){ // if fork failed
         fprintf(stderr, "ERROR: Failed to create dispatcher process\n");
         free(config);
+        free(cart_pids);
+        free(visitor_pids);
         destroy_semaphores(shared_mem);
         destroy_shared_memory(shared_name, shared_id, shared_mem);
         exit(1);
@@ -126,21 +145,29 @@ int main (int argc, char** argv){
         pid_t cart_pid = fork();
         if (cart_pid < 0){ // if fork failed
             fprintf(stderr, "ERROR: Failed to create cart process\n");
+            int carts_to_abort = shared_mem->cart_cnt;
             for (int cnt = 0; cnt < (config->visitor_num / config->cart_capacity + 1); cnt++){ // eliminate inner counter inside dispatcher
                 sem_post(&shared_mem->cart_disp);
             }
             sem_post(&shared_mem->write_mutex);
             waitpid(disp_pid, NULL, 0); // wait for dispatcher to abort
+            sem_wait(&shared_mem->write_mutex);
             for (int cnt = 0; cnt < shared_mem->cart_cnt * config->cart_capacity; cnt++){ // sends enough signals to get all carts to the end
                 sem_post(&shared_mem->boarding);
                 sem_post(&shared_mem->unboarding);
             }
-            while(waitpid(-1, NULL, 0) > 0); // wait for all subprocesses to abort
+            sem_post(&shared_mem->write_mutex);
+            for (int idx = 0; idx < carts_to_abort; idx++){ // wait for all carts to abort
+                waitpid(cart_pids[idx], NULL, 0);
+            }
             free(config);
+            free(cart_pids);
+            free(visitor_pids);
             destroy_semaphores(shared_mem);
             destroy_shared_memory(shared_name, shared_id, shared_mem);
             exit(1);
         } else if (cart_pid == 0){
+            cart_pids[shared_mem->cart_cnt] = cart_pid;
             shared_mem->cart_cnt++;
             cart(shared_mem->cart_cnt, shared_mem, config); // shared_mem->write_mutex is passed to cart
         }
@@ -150,19 +177,52 @@ int main (int argc, char** argv){
         pid_t visitor_pid = fork();
         if (visitor_pid < 0){ // if fork failed
             fprintf(stderr, "ERROR: Failed to create visitor process\n");
-            // TODO: signal already existing processes to abort
-            waitpid(disp_pid, NULL, 0); // wait for dispatcher to end
+            int carts_to_abort = shared_mem->cart_cnt;
+            int visitors_to_abort = shared_mem->visitor_cnt;
+            for (int cnt = 0; cnt < visitors_to_abort; cnt++){
+                sem_post(&shared_mem->turnstile_enter);
+                sem_post(&shared_mem->turnstile_leave);
+            }
+            sem_post(&shared_mem->write_mutex);
+            for (int idx = 0; idx < visitors_to_abort; idx++){ // wait for all visitors to abort
+                waitpid(visitor_pids[idx], NULL, 0);
+            }
+            sem_wait(&shared_mem->write_mutex);
+            for (int cnt = 0; cnt < (config->visitor_num / config->cart_capacity + 1); cnt++){ // eliminate inner counter inside dispatcher
+                sem_post(&shared_mem->cart_disp);
+            }
+            sem_post(&shared_mem->write_mutex);
+            waitpid(disp_pid, NULL, 0); // wait for dispatcher to abort
+            sem_wait(&shared_mem->write_mutex);
+            for (int cnt = 0; cnt < shared_mem->cart_cnt * config->cart_capacity; cnt++){ // sends enough signals to get all carts to the end
+                sem_post(&shared_mem->boarding);
+                sem_post(&shared_mem->unboarding);
+            }
+            sem_post(&shared_mem->write_mutex);
+            for (int idx = 0; idx < carts_to_abort; idx++){ // wait for all carts to abort
+                waitpid(cart_pids[idx], NULL, 0);
+            }
             free(config);
+            free(cart_pids);
+            free(visitor_pids);
             destroy_semaphores(shared_mem);
             destroy_shared_memory(shared_name, shared_id, shared_mem);
             exit(1);
         } else if (visitor_pid == 0){
+            cart_pids[shared_mem->visitor_cnt] = visitor_pid;
             shared_mem->visitor_cnt++;
             visitor(shared_mem->visitor_cnt, shared_mem, config); // shared_mem->write_mutex is passed to visitor
         }
     }
-    while(waitpid(-1, NULL, 0) > 0); // wait for all subprocesses to end
-    waitpid(disp_pid, NULL, 0); // wait for dispatcher to end just for sure
+    for (int idx = 0; idx < config->visitor_num; idx++){ // wait for all visitors to abort
+        waitpid(visitor_pids[idx], NULL, 0);
+    }
+    for (int idx = 0; idx < config->cart_num; idx++){ // wait for all carts to abort
+        waitpid(cart_pids[idx], NULL, 0);
+    }
+    waitpid(disp_pid, NULL, 0); // wait for dispatcher to end
+    free(cart_pids);
+    free(visitor_pids);
     free(config);
     destroy_semaphores(shared_mem);
     destroy_shared_memory(shared_name, shared_id, shared_mem);
